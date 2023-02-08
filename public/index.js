6476a7ba6f9d1fe77178a16e715f26de45140deb
DJFancyAl <abluemleiv@gmail.com>
2023-02-08 12:01:10 -0500

    Completed All 3 Charts & API fetch works

--- public/index.js
+++ public/index.js
@@ -1,8 +1,90 @@
-async function main() {
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
 
+function getHigh(stock){
+    let high = 0;
+    stock.values.forEach(value => {
+        if (value.high > high){
+            high = value.high
+        }
+    })
+    return high
+}
+
+function getAverage(stock){
+    let total = 0
+    stock.values.forEach(value => {
+        total += parseFloat(value.high)
+    })
+    return total/stock.values.length
+}
+
+
+async function main() {
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+
+    let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=13ec509bae7343f3a4fa9ca56d92e84d')
+    let result = await response.json()
+
+    const {GME,MSFT,DIS,BNTX} = result
+    const stocks = [GME, MSFT, DIS, BNTX]
+    stocks.forEach( stock => stock.values.reverse())
+    console.log(stocks)
+
+    let timeChart = new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map( stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.map(value => parseFloat(value.high)),
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol)
+            }))
+        }
+    });
+
+    let highChart = new Chart(highestPriceChartCanvas.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: "High Value",
+                data: stocks.map(stock => getHigh(stock)),
+                backgroundColor: stocks.map(stock => getColor(stock.meta.symbol)),
+                borderColor: stocks.map(stock => getColor(stock.meta.symbol))
+
+            }]
+        }
+    });
+
+    let averageChart = new Chart(averagePriceChartCanvas.getContext('2d'), {
+        type: 'pie',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: "Average High",
+                data: stocks.map(stock => getAverage(stock)),
+                backgroundColor: stocks.map(stock => getColor(stock.meta.symbol)),
+                borderColor: stocks.map(stock => getColor(stock.meta.symbol))
+
+            }]
+        }
+    });
 
 }
 
